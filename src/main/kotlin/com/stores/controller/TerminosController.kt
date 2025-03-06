87531bc627450151f8f7130463b51ea42199a85c
DIEGO EDGAR JUAREZ ELIZALDE <diegoedgar69@gmail.com>
2025-03-06 13:38:50 -0600

    se deja casi completa la parte de los reportes

--- src/main/kotlin/com/stores/controller/TerminosController.kt
+++ src/main/kotlin/com/stores/controller/TerminosController.kt
@@ -10,28 +10,19 @@ import org.springframework.web.bind.annotation.RequestMapping
 @RequestMapping("terminos-condiciones")
 class TerminosController {
 
-
     @GetMapping("veterinaria/luna")
     fun lunaVet(model: Model): String {
-        model.addAttribute("telefono", "7442130868")
-            .addAttribute("direccion", "Av. Las Torres Etapa 38, coloso ruta axtel, El Coloso, 39810 Acapulco de Juárez, Gro.")
-            .addAttribute("nombre", "Luna Vet")
+        model.addAttribute("telefono", "7442130868").addAttribute(
+            "direccion", "Av. Las Torres Etapa 38, coloso ruta axtel, El Coloso, 39810 Acapulco de Juárez, Gro."
+        ).addAttribute("nombre", "Luna Vet")
         return "plantillaVeterinaria"
     }
 
     @GetMapping("veterinaria/safari")
-    fun safariVet(model: Model): String  {
+    fun safariVet(model: Model): String {
         model.addAttribute("telefono", "7444557430")
             .addAttribute("direccion", "Peña Blanca, El Coloso, 39810 Acapulco de Juárez, Gro.")
             .addAttribute("nombre", "Safari Vet")
-        return "plantillaVeterinaria"
-    }
-
-    @GetMapping("veterinaria/perro")
-    fun camaDelPerro(model: Model):String  {
-        model.addAttribute("telefono", "7442130868")
-            .addAttribute("direccion", "Coloso avenida las torres etapa 38")
-            .addAttribute("nombre", "Luna Vet")
         return "plantillaVeterinaria"
     }
 }
